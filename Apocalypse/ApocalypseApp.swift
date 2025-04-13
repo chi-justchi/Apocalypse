c1bca0c184883d2f73fb0b4e9b48d261e683c8fc
chi-justchi <chivo@usf.edu>
2025-04-13 02:47:57 -0400

    Initial Commit

---
+++ Apocalypse/ApocalypseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApocalypseApp.swift
+//  Apocalypse
+//
+//  Created by Chi Vo on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ApocalypseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
